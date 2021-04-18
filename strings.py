ec43d67e27faba89b29912474e68e0039a672406
kishorepalem20 <kishorepalem20@gmail.com>
2021-04-18 11:29:27 +0530

    added escapechar and strings

---
+++ strings.py
@@ -0,0 +1,12 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " World")
+greeting = "Hello"
+name = "Kishore"
+
+#if we want a space, we can add that too
+print(greeting + ' ' + name)
+name1 = input("Please enter your name \n")
+print(greeting + ' ' + name1)
